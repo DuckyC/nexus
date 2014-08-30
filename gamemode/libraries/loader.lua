9c1e94213c185d34c9a723191baf7fcc4b36f719
arcaneex <epic@funny.cat>
2014-08-31 01:51:28 +0300

    Forgot loader

---
+++ gamemode/libraries/loader.lua
@@ -0,0 +1,21 @@
+local path = (GM and GM.Path) or GAMEMODE.Path
+
+function nexus.load()
+	for _, x in pairs(file.Find(path..'/libraries/*','GAME')) do
+		local realm = x:sub(1,2)
+		local relPath = 'nexus/gamemode/libraries/'..x
+		if realm == 'cl' then
+			if SERVER then AddCSLuaFile(relPath) print('Sent '..x) continue end
+			include(relPath)
+			print('Included '..x)
+		elseif realm == 'sv' then
+			if SERVER then include(relPath) print('Included '..x) end
+		elseif realm =='sh' then
+			if SERVER then AddCSLuaFile(relPath) print('Sent '..x) end
+			include(relPath)
+			print('Included '..x)
+		end
+	end
+end
+
+nexus.load()

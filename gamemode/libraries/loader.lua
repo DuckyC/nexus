cc6183b260295adc30945ff77e58466681c7286a
arcaneex <epic@funny.cat>
2014-08-31 03:36:47 +0300

    Updated loader

--- gamemode/libraries/loader.lua
+++ gamemode/libraries/loader.lua
@@ -4,16 +4,15 @@ function nexus.load()
 	print("Loading libraries")
 	for _, x in pairs(file.Find(path..'/libraries/*','GAME')) do
 		local realm = x:sub(1,2)
-		local relPath = GM.FolderName..'/gamemode/libraries/'..x
 		if realm == 'cl' then
-			if SERVER then AddCSLuaFile(relPath) print('Sent '..x) continue end
-			include(relPath)
+			if SERVER then AddCSLuaFile(x) print('Sent '..x) continue end
+			include(x)
 			print('Included '..x)
 		elseif realm == 'sv' then
-			if SERVER then include(relPath) print('Included '..x) end
+			if SERVER then include(x) print('Included '..x) end
 		elseif realm =='sh' then
-			if SERVER then AddCSLuaFile(relPath) print('Sent '..x) end
-			include(relPath)
+			if SERVER then AddCSLuaFile(x) print('Sent '..x) end
+			include(x)
 			print('Included '..x)
 		end
 	end

9fd51c8626147a6c4d93a857fab8ea2da1e126e9
crazyscouter <crazyscouter1@gmail.com>
2014-09-03 14:45:33 -0700

    Fix issues with some clients.

--- gamemode/libraries/loader.lua
+++ gamemode/libraries/loader.lua
@@ -2,18 +2,27 @@ local path = (GM and GM.Path) or GAMEMODE.Path
 
 function nexus.load()
 	print("Loading libraries")
-	for _, x in pairs(file.Find(path..'/libraries/*','GAME')) do
-		local realm = x:sub(1,2)
-		if realm == 'cl' then
-			if SERVER then AddCSLuaFile(x) print('Sent '..x) continue end
-			include(x)
-			print('Included '..x)
-		elseif realm == 'sv' then
-			if SERVER then include(x) print('Included '..x) end
-		elseif realm =='sh' then
-			if SERVER then AddCSLuaFile(x) print('Sent '..x) end
-			include(x)
-			print('Included '..x)
+	if (SERVER) then
+		for _, x in pairs(file.Find(path..'/libraries/*','GAME')) do
+			if (x == "loader.lua") then continue end
+
+			local realm = x:sub(1,2)
+			if realm == 'cl' then
+				AddCSLuaFile(x) print('Sent '..x)
+			elseif realm == 'sv' then
+				include(x) print('Included '..x)
+			elseif realm =='sh' then
+				AddCSLuaFile(x) print('Sent '..x)
+				include(x)
+			end
+		end
+	else
+		local fil = file.Find("nexus/gamemode/libraries/*", "LUA") //blah blah use this instead of path. path includes the gamemodes folder
+		for k,v in pairs(fil) do
+			if (v == "loader.lua") then continue end
+
+			print(v) 
+			include(v)
 		end
 	end
 end
